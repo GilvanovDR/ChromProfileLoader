1422051a2ae85103ba48b83410cdfd148ebebc98
lucefer13 <lucefer13+github@gmail.com>
2020-01-21 10:12:57 +0500

    Initial commit

---
+++ src/main/java/ru/GilvanovDR/ChromeProfileLoader/MainApp.java
@@ -0,0 +1,27 @@
+/*
+ * GilvanovDR (c) 2020.
+ */
+
+package ru.GilvanovDR.ChromeProfileLoader;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/hello.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("ChromeLoader by GilvanovDR");
+        stage.setScene(new Scene(root));
+        stage.resizableProperty().setValue(false);
+        stage.show();
+    }
+}
---
+++ src/main/java/ru/GilvanovDR/ChromeProfileLoader/controller/Controller.java
@@ -0,0 +1,66 @@
+/*
+ * GilvanovDR (c) 2020.
+ */
+
+package ru.GilvanovDR.ChromeProfileLoader.controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Controller {
+
+    private final static File profileListPath = new File("C:\\Users\\termuser\\Desktop\\ChromeProfile");
+    private final static String chromeExecPath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+
+
+    @FXML
+    private ListView<String> list;
+
+    @FXML
+    public void initialize() {
+        list.setItems(getProfileList());
+
+    }
+
+    @FXML
+    public void onClickLoad(MouseEvent mouseEvent) {
+        String text;
+        if ((text = list.getFocusModel().getFocusedItem()) != null) {
+            runApplication(text);
+            System.exit(0);
+        }
+    }
+
+    private void runApplication(String text) {
+        String execArgs = "--user-data-dir=" + profileListPath.getPath() + "\\" + text;
+        ProcessBuilder builder = new ProcessBuilder(chromeExecPath, execArgs);
+        try {
+            Process process = builder.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            ;
+        }
+    }
+
+    private ObservableList getProfileList() {
+        ArrayList<String> ls = new ArrayList<String>();
+        System.out.println();
+        for (File file1 : Objects.requireNonNull(profileListPath.listFiles())) {
+            if (file1.isDirectory()) {
+                ls.add(file1.getName());
+            }
+        }
+        System.out.println();
+        return FXCollections.observableArrayList(ls);
+    }
+
+
+}
